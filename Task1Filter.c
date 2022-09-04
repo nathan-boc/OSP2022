c12bbd155efd322a69e88178de6e84018973f9d3
Nathan12313 <41312297+nathan-boc@users.noreply.github.com>
2022-09-04 20:22:43 +1000

    Setup Task1Filter.c

---
+++ Task1Filter.c
@@ -0,0 +1,33 @@
+void Task1Filter(char wordlistFolder[], char cleanWords[]) {
+
+// Access wordlist folder for the required files
+
+
+// Combine words from all lists into an array
+
+
+// Trim leading and trailing whitespace
+
+// Sort into alphabetical order
+
+
+// Remove duplicates
+
+// Remove words containing punctuation
+
+// Remove words outside the range 3-15, inclusive
+
+// Remove strings containing numerals
+
+// Clean blank lines
+
+
+// Shuffle array of words
+
+
+// Output cleaned wordlist to cleanWords.txt
+	// One word per line
+
+}
+
+
